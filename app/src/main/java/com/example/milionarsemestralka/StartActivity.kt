7773d289db1cecb9ffded0fd27d89a1a9107941e
Martin Šimko <martin.simko.23@gmail.com>
2025-04-27 22:41:12 +0200

    added PrizeLadderActivity

--- app/src/main/java/com/example/milionarsemestralka/StartActivity.kt
+++ app/src/main/java/com/example/milionarsemestralka/StartActivity.kt
@@ -1,23 +1,37 @@
 package com.example.milionarsemestralka
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.milionarsemestralka.ui.theme.MilionarSemestralkaTheme
 
 class StartActivity : ComponentActivity() {
@@ -27,14 +41,17 @@ class StartActivity : ComponentActivity() {
         setContent {
             MilionarSemestralkaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { outerPadding ->
+
                     BackgroundImage(
                         modifier = Modifier.padding(outerPadding)
                     )
+
                     Buttons(
                         modifier = Modifier.padding(outerPadding)
                     )
                 }
             }
+            LadderActivityTransition();
         }
     }
 }
@@ -54,15 +71,15 @@ fun BackgroundImage(modifier: Modifier = Modifier) {
     )
 }
 
-
 @Composable
-fun Buttons(modifier: Modifier = Modifier) {
-    val buttonImage = painterResource(R.drawable.menubutton)
+fun LogoImage(modifier: Modifier = Modifier) {
     val logoImage = painterResource(R.drawable.logo)
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center//https://developer.android.com/codelabs/basic-android-kotlin-compose-add-images#5
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center
+
     ) {
         Image(
             painter = logoImage,
@@ -70,32 +87,110 @@ fun Buttons(modifier: Modifier = Modifier) {
             modifier = Modifier
                 .fillMaxWidth(1f)
                 .aspectRatio(16f / 9f) // chatgpt modifier
-
         )
-        Image(
-            painter = buttonImage,
-            contentDescription = null,
-            modifier = Modifier
-                .fillMaxWidth(0.8f)
-                .aspectRatio(16f / 9f) // chatgpt modifier
-        )
-        Image(
-            painter = buttonImage,
-            contentDescription = null,
-            modifier = Modifier
-                .fillMaxWidth(0.8f)
-                .aspectRatio(16f / 9f) // chatgpt modifier
-        )
-        Image(
-            painter = buttonImage,
-            contentDescription = null,
-            modifier = Modifier
-                .fillMaxWidth(0.8f)
-                .aspectRatio(16f / 9f) // chatgpt modifier
-        )
-
     }
 }
+
+
+@Composable
+fun Buttons(modifier: Modifier = Modifier) {
+    val buttonImage = painterResource(R.drawable.menubutton)
+    val logoImage = painterResource(R.drawable.logo)
+    val buttonModifier = Modifier
+        .fillMaxWidth(1f)
+        .scale(1.5f)
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp)
+                .align(Alignment.TopCenter),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = logoImage,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth(1f)
+                    .fillMaxHeight(0.3f)
+            )
+        }
+        Spacer(modifier = Modifier.height(150.dp))
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly,
+
+        ) {/*
+            Image(
+                painter = buttonImage,
+                contentDescription = null,
+                modifier = buttonModifier
+            )
+            Text(text = "Button 1", modifier = buttonModifier, fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            */
+            Button(
+                onClick = { /* handle click */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .scale(1.5f),
+                content = {
+                    Image(
+                        painter = painterResource(R.drawable.menubutton),
+                        contentDescription = null,
+                        modifier = Modifier,
+                        contentScale = ContentScale.FillBounds
+                    )
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent
+                )
+            )
+
+            Spacer(modifier = Modifier.height(50.dp))
+            Button(
+                onClick = { /* handle click */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .scale(1.5f),
+                content = {
+                    Image(
+                        painter = painterResource(R.drawable.menubutton),
+                        contentDescription = null,
+                        modifier = Modifier,
+                        contentScale = ContentScale.FillBounds
+                    )
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent
+                )
+            )
+            Spacer(modifier = Modifier.height(50.dp))
+            Button(
+                onClick = { /* handle click */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .scale(1.5f),
+                content = {
+                    Image(
+                        painter = painterResource(R.drawable.menubutton),
+                        contentDescription = null,
+                        modifier = Modifier,
+                        contentScale = ContentScale.FillBounds
+                    )
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent
+                )
+            )
+        }
+    }
+}
+
 
 
 @Composable
@@ -104,5 +199,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun LadderActivityTransition() {
+    val context = LocalContext.current
+    val intent = Intent(context, HotSeatActivity::class.java)
+    context.startActivity(intent)
+
 }
 
